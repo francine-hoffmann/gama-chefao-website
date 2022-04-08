0740dd8a759c186864743f2dd66ab864279e8d25
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-08 13:33:56 -0300

    Adicao de porta ao proxy

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Routes from './Routes';
+import GlobalStyles from './assets/styles/Global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />      
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
