8d41d61a318dade5e31487f5b06945f148f369ed
Francine Hoffmann <francine.bhoffmann@gmail.com>
2022-04-08 13:41:10 -0300

    Adicao de porta ao proxy

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ app.use(routes);
 const PORT = process.env.PORT || 3000;
 
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
 
